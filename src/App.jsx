eb9db9da73e54eeb8726a061c2375fe4bf178609
dekin <dekinlinux@gmail.com>
2023-06-15 01:44:35 +0000

    blue effect nad colors

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Sidebar from './components/Sidebar'
 function App() {
   return (
     <div className='md:flex flex-wrap justify-center items-center h-screen p-5 bg-image'>
-      <div className='container md:w-[50%] md:mx-auto p-2  shadow-blue-300 flex flex-wrap rounded-xl h-full border-[3px] border-white shadow-lg overflow-auto bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50'>
+      <div className='container md:w-[50%] md:mx-auto p-2  shadow-blue-300 flex flex-wrap rounded-xl h-full border-[3px] border-white shadow-lg overflow-auto bg-white bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-50'>
         <Center/>
       </div>
 
