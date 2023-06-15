c2973f5667502487bcfe194f3975dfde2a70a13e
dekin <dekinlinux@gmail.com>
2023-06-15 01:39:04 +0000

    adding background image

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className='md:flex flex-wrap justify-center items-center h-screen p-5'>
-      <div className='container md:w-[50%] md:mx-auto p-2 bg-white shadow-blue-300 flex flex-wrap rounded-xl h-full border-[3px] border-blue-500 shadow-lg overflow-auto'>
+    <div className='md:flex flex-wrap justify-center items-center h-screen p-5 bg-image'>
+      <div className='container md:w-[50%] md:mx-auto p-2  shadow-blue-300 flex flex-wrap rounded-xl h-full border-[3px] border-white shadow-lg overflow-auto bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50'>
         <Center/>
       </div>
 
